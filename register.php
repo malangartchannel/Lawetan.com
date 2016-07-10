3a540b62c2fa65b2ca9be1d977c9b85c94371395
Malang Art Channel <malangartchannel2015@gmail.com>
2016-07-11 02:04:28 +0700

    Update register.php

--- register.php
+++ register.php
@@ -13,7 +13,7 @@
 
 // Cek syarat minimal versi PHP
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
-    exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
+    exit("Maaf, Simple PHP Login tidak berjalan pada versi PHP dibawah 5.3.7 !");
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     // jika Anda menggunakan PHP 5.3 atah PHP 5.4 Anda harus mengikutkan password_api_compatibility_library.php
     // (library ini menambahkan fungsi 'password hashing' PHP 5.5 ke versi lama PHP)
