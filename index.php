a9d64b0f3300e0ebc4ea5c308c99bb10bc82f220
Malang Art Channel <malangartchannel2015@gmail.com>
2016-07-11 11:26:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
  * @license http://opensource.org/licenses/MIT MIT License
  */
 
-// checking for minimum PHP version
+// cek versi minimum PHP
 if (version_compare(PHP_VERSION, '5.3.7', '<')) {
-    exit("Sorry, Simple PHP Login does not run on a PHP version smaller than 5.3.7 !");
+    exit("Maaf, Simple PHP Login tidak bisa berjalan di PHP versi dibawah 5.3.7 !");
 } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
     // if you are using PHP 5.3 or PHP 5.4 you have to include the password_api_compatibility_library.php
     // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
